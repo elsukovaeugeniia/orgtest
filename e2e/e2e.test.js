7a52378917e6862fdf71fce32c841ee99fc2ee5e
Elsukova Eugeniia <elsukova.eugeniia@gmail.com>
2026-03-08 21:10:43 +0300

    Fix CI workflow: add Playwright browser installation and Browserslist update

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -17,7 +17,7 @@ describe('Credit Card Validator form', () => {
       });
     });
 
-    browser = await chromium.launch({ headless: true });
+    browser = await chromium.launch({ headless: false });
     page = await browser.newPage();
     await page.goto(baseUrl, { waitUntil: 'networkidle' });
   }, 60000); // Таймаут для beforeAll
